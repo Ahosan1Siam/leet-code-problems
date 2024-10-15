838d37b15af39dc79929e5c85a012e2dba7ff46d
Ahosan Rahman Siam <39805225+Ahosan1Siam@users.noreply.github.com>
2024-10-15 20:32:15 +0600

    hot fix

--- reverseLinkedList.java
+++ reverseLinkedList.java
@@ -14,10 +14,10 @@ public class reverseLinkedList {
     }
 }
 
-class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
+//class ListNode {
+//      int val;
+//      ListNode next;
+//      ListNode() {}
+//      ListNode(int val) { this.val = val; }
+//      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//  }
